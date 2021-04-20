2ce9a1f96f999b628146c648eab907036e1de33e
Dominick Pack <dominick.pack1@t-mobile.com>
2021-04-20 15:19:52 -0700

    loading state

--- App.js
+++ App.js
@@ -1,13 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, SafeAreaView, ActivityIndicator, FlatList } from 'react-native';
 
 export default function App() {
+
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({"name":"xyz"});
+  
+  const [state, setState] = useState({myText: "Hello!"});
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  var requestOptions = {
+    method: 'PUT',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+  const changeTextValue = () => {
+    setState({myText: "Changed Text!"});
+  }
+
+  useEffect(() => {
+    setTimeout(function(){ 
+      fetch("https://607a0ad7bd56a60017ba264c.mockapi.io/device/1", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+    }, 3000);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>{state.myText}</Text>
+      <Button title="Text Changer" onPress={changeTextValue} color="magenta"/>
+
+      
+      
+      {isLoading ? <ActivityIndicator size="large" color="#0000ff" /> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.title}, {item.releaseYear}</Text>
+          )}
+        />
+      )}
+      
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -17,5 +61,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 24
   },
 });
