a760d0b8b2d3e2e730cf83db0965dfca0dd3fb23
Daniel Lee <>
2021-04-24 10:13:56 -0700

    added hook

--- App.js
+++ App.js
@@ -1,57 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, SafeAreaView, ActivityIndicator, FlatList } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  SafeAreaView,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
 
 export default function App() {
 
   var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append('Content-Type', 'application/json');
 
-  var raw = JSON.stringify({"name":"xyz"});
-  
-  const [state, setState] = useState({myText: "Hello!"});
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
+  var raw = JSON.stringify({'name': 'xyz'});
+
+  const [state, setState] = useState({myText: 'Hello!'}); // state hook
+  const [isLoading, setLoading] = useState(true); // state hook
+  const [data, setData] = useState([]); // state hook
 
   var requestOptions = {
     method: 'PUT',
     headers: myHeaders,
     body: raw,
-    redirect: 'follow'
+    redirect: 'follow',
   };
 
   const changeTextValue = () => {
-    setState({myText: "Changed Text!"});
-  }
+    setState({myText: 'Changed Text!'});
+  };
 
-  useEffect(() => {
-    setTimeout(function(){ 
-      fetch("https://607a0ad7bd56a60017ba264c.mockapi.io/device/1", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
+  useEffect(() => { // the component has been loaded (life cycle of the component)
+
+    setTimeout(async function() { // simulate Fetch request delay
+      fetch('https://607a0ad7bd56a60017ba264c.mockapi.io/device/1',
+          requestOptions) // the call to fetch data
+          .then(
+              response => response.text()) // promise to handle/process the data https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+
+          .then(result => {
+            setLoading(false);
+            result.key = result.id;
+            setData([result]);
+            console.log(result);
+          }) // promise to handle/process the data
+
+          .catch(error => console.log('error', error));
+
     }, 3000);
+
   }, []);
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>{state.myText}</Text>
-      <Button title="Text Changer" onPress={changeTextValue} color="magenta"/>
+  const renderItem = ({item}) => {
+    console.log(item)
+    return <Text key={item.id}>
+      Hello {item.name}
+    </Text>;
+  };
 
-      
-      
-      {isLoading ? <ActivityIndicator size="large" color="#0000ff" /> : (
-        <FlatList
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-            <Text>{item.title}, {item.releaseYear}</Text>
-          )}
-        />
-      )}
-      
-      <StatusBar style="auto" />
-    </SafeAreaView>
+  return (
+      <SafeAreaView style={styles.container}>
+        <Text>{state.myText}</Text>
+        <Button title="Text Changer" onPress={changeTextValue} color="magenta"/>
+
+
+        {isLoading ? <ActivityIndicator size="large" color="#0000ff"/> : (
+            <FlatList
+                data={data}
+                keyExtractor={(id, index) => id}
+                renderItem={renderItem}
+            />
+        )}
+
+        <StatusBar style="auto"/>
+      </SafeAreaView>
   );
 }
 
@@ -61,6 +85,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 24
+    padding: 24,
   },
 });
