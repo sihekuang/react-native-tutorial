071c1e31cd68675d759119a68cef902417a754ee
Daniel Lee <>
2021-04-24 11:08:33 -0700

    updated the tutorial with more comments

--- App.js
+++ App.js
@@ -7,57 +7,57 @@ import {
   Button,
   SafeAreaView,
   ActivityIndicator,
-  FlatList,
+  FlatList, Switch,
 } from 'react-native';
 
 export default function App() {
 
-  var myHeaders = new Headers();
-  myHeaders.append('Content-Type', 'application/json');
-
-  var raw = JSON.stringify({'name': 'xyz'});
-
+  // Setting up REST API call
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+  };
+  // React hooks
   const [state, setState] = useState({myText: 'Hello!'}); // state hook
   const [isLoading, setLoading] = useState(true); // state hook
   const [data, setData] = useState([]); // state hook
 
-  var requestOptions = {
-    method: 'PUT',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow',
-  };
-
-  const changeTextValue = () => {
-    setState({myText: 'Changed Text!'});
-  };
-
+  /**
+   * UseEffect consumes a method and that method is called when the React Component is loaded on the UI
+   */
   useEffect(() => { // the component has been loaded (life cycle of the component)
 
-    setTimeout(async function() { // simulate Fetch request delay
-      fetch('https://607a0ad7bd56a60017ba264c.mockapi.io/device/1',
-          requestOptions) // the call to fetch data
-          .then(
-              response => response.text()) // promise to handle/process the data https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
-
-          .then(result => {
-            setLoading(false);
-            result.key = result.id;
-            setData([result]);
-            console.log(result);
-          }) // promise to handle/process the data
-
-          .catch(error => console.log('error', error));
-
-    }, 3000);
+    /**
+     * Do a REST Api call
+     */
+    fetch('https://607a0ad7bd56a60017ba264c.mockapi.io/device',
+        requestOptions)// the call to fetch data
+        // doing work xyz
+        .then( // when the response comes back
+            response => response.json()) // promise to handle/process the data https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+        .then(result => {
+          setLoading(false);
+          setData(result);
+          console.log(result);
+        }) // promise to handle/process the data
+        .catch(error => console.log('error', error));
 
   }, []);
 
   const renderItem = ({item}) => {
-    console.log(item)
-    return <Text key={item.id}>
-      Hello {item.name}
-    </Text>;
+    return <View>
+      <Text key={item.id}>
+        Device Name: {item.name}
+      </Text>
+      <Switch value={item.isOn} />
+    </View>;
+  };
+
+  /**
+   * Changes the "state" that is declared in the hook
+   */
+  const changeTextValue = () => {
+    setState({myText: 'Changed Text!'});
   };
 
   return (
@@ -69,7 +69,7 @@ export default function App() {
         {isLoading ? <ActivityIndicator size="large" color="#0000ff"/> : (
             <FlatList
                 data={data}
-                keyExtractor={(id, index) => id}
+                keyExtractor={(id, index) => `${index}`}
                 renderItem={renderItem}
             />
         )}
